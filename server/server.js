5e4ce4047d6819a225b19e899ee7a97859fc65b1
Shreyas <sshreyasju@gmail.com>
2025-07-16 21:12:20 +0530

    trying in render

--- server/server.js
+++ server/server.js
@@ -48,10 +48,11 @@ app.use("/api/messages", messageRouter);
 // Connect to MongoDB
 await connectDB();
 
-if (process.env.NODE_ENV !== "production") {
-  const PORT = process.env.PORT || 5000;
-
-  server.listen(PORT, () => console.log("Server is running on PORT " + PORT));
-}
+// if (process.env.NODE_ENV !== "production") {
+//   const PORT = process.env.PORT || 5000;
+//   server.listen(PORT, () => console.log("Server is running on PORT " + PORT));
+// }
 // Export server for vercel
-export default server;
+// export default server;
+  const PORT = process.env.PORT || 5000;
+  server.listen(PORT, () => console.log("Server is running on PORT " + PORT));
